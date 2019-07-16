04e43db86c4cb7fe084c696059af599f37b02851
Safin Timur <timurtlt96@mail.ru>
2019-07-16 23:52:07 +0300

    Created cli

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,28 @@
+import argparse
+import os
+
+
+def main():
+    parser = create_parser()
+    namespace = parser.parse_args()
+    if not namespace.path:
+        exit('Укажите правильный путь каталога')
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(prefix_chars='-+/')
+    parser.add_argument('path',
+                        nargs='?',
+                        default=False,
+                        type=is_directory,
+                        help='Path to directory')
+    return parser
+
+
+def is_directory(path):
+    if os.path.isdir(path):
+        return path
+
 
 if __name__ == '__main__':
-    pass
+    main()
