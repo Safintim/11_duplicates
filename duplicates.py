6b672fbfbf1086e086d5f87d98dedc376e3da308
Safin Timur <timurtlt96@mail.ru>
2019-07-18 22:26:35 +0300

    Added print_duplicates

--- duplicates.py
+++ duplicates.py
@@ -6,7 +6,8 @@ from collections import defaultdict
 def main():
     parser = create_parser()
     namespace = parser.parse_args()
-    print('\n'.join(get_duplicate_files(namespace.path)))
+    duplicates = get_duplicate_files(namespace.path)
+    print_duplicates(duplicates)
 
 
 def create_parser():
@@ -27,8 +28,7 @@ def get_duplicate_files(path):
     duplicates = []
     for filename, filepaths in find_files_with_same_name(path).items():
         if len(filepaths) > 1:
-            duplicates.append('\n')
-            duplicates.extend(filepaths)
+            duplicates.append(filepaths)
     return duplicates
 
 
@@ -40,6 +40,12 @@ def find_files_with_same_name(path):
             filename_filesize = (filename, os.path.getsize(filepath))
             files[filename_filesize].append(filepath)
     return files
+
+
+def print_duplicates(duplicates):
+    for filepaths in duplicates:
+        print('\n')
+        print('\n'.join(filepath for filepath in filepaths))
 
 
 if __name__ == '__main__':
